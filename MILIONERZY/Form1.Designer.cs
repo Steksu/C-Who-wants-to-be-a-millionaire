586dbdcf2c47da9009cb12c606e9094fc7276fe0
Steksu <94147506+Steksu@users.noreply.github.com>
2024-02-21 02:20:39 +0100

    Add files via upload

---
+++ MILIONERZY/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace MILIONERZY
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            button3 = new System.Windows.Forms.Button();
+            button4 = new System.Windows.Forms.Button();
+            label1 = new System.Windows.Forms.Label();
+            button5 = new System.Windows.Forms.Button();
+            listBox1 = new System.Windows.Forms.ListBox();
+            button6 = new System.Windows.Forms.Button();
+            button7 = new System.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(143, 299);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(107, 50);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(387, 299);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(97, 50);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new System.Drawing.Point(143, 391);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(107, 47);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new System.Drawing.Point(387, 391);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(97, 47);
+            button4.TabIndex = 3;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label1.Location = new System.Drawing.Point(143, 42);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(78, 32);
+            label1.TabIndex = 5;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new System.Drawing.Point(679, 102);
+            button5.Name = "button5";
+            button5.Size = new System.Drawing.Size(75, 60);
+            button5.TabIndex = 7;
+            button5.Text = "50/50";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Items.AddRange(new object[] { "1000000", "500000", "250000", "125000", "75000", "40000", "20000", "10000", "5000", "2000", "1000", "500" });
+            listBox1.Location = new System.Drawing.Point(906, 58);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new System.Drawing.Size(159, 364);
+            listBox1.TabIndex = 8;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // button6
+            // 
+            button6.Location = new System.Drawing.Point(679, 358);
+            button6.Name = "button6";
+            button6.Size = new System.Drawing.Size(110, 44);
+            button6.TabIndex = 9;
+            button6.Text = "Zakończ grę";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new System.Drawing.Point(679, 179);
+            button7.Name = "button7";
+            button7.Size = new System.Drawing.Size(75, 59);
+            button7.TabIndex = 10;
+            button7.Text = "widownia";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1093, 450);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(listBox1);
+            Controls.Add(button5);
+            Controls.Add(label1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ MILIONERZY/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static MILIONERZY.Form1;
+
+namespace MILIONERZY
+{
+    public partial class Form1 : Form
+    {
+        private List<Pytanie> pytania;
+        private int PytanieIndex = 0;
+        private int OdpowiedzIndex;
+        public Form1()
+        {
+            InitializeComponent();
+            DodajPytania();
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = true;
+            if (PytanieIndex > pytania.Count - 1)
+            {
+                MessageBox.Show($"Gratulacje, twoja wygrana to 1000000$");
+                Close();
+            }
+            else
+            {
+                Pytanie pytanie1 = pytania[PytanieIndex];
+                label1.Text = pytanie1.Text;
+                button1.Text = "A: " + pytanie1.Odpowiedz[0];
+                button2.Text = "B: " + pytanie1.Odpowiedz[1];
+                button3.Text = "C: " + pytanie1.Odpowiedz[2];
+                button4.Text = "D: " + pytanie1.Odpowiedz[3];
+            }
+
+
+
+        }
+        public class Pytanie
+        {
+            public string Text;
+            public List<string> Odpowiedz;
+            public int PytanieIndex;
+
+            public Pytanie(string Text, List<string> Odpowiedz, int PytanieIndex)
+            {
+                this.Text = Text;
+                this.Odpowiedz = Odpowiedz;
+                this.PytanieIndex = PytanieIndex;
+            }
+        }
+
+        private void DodajPytania()
+        {
+            pytania = new List<Pytanie>
+            {
+                new Pytanie("Jaka jest stolica Francji?", new List<string> { "Berlin", "Paryż", "Londyn", "Madryt" }, 1),
+                new Pytanie("Która planeta jest znana jako czerwona planeta?", new List<string> { "Mars", "Venus", "Jupiter", "Saturn" }, 0),
+                new Pytanie("Jaki jest największy ssak na ziemi?", new List<string> { "Słoń", "Płetwal błękitny", "Żyrafa", "Goryl" }, 1),
+                new Pytanie("W którym roku zakończyła się II wojna światowa?", new List<string> { "1943", "1945", "1950", "1939" }, 1),
+                new Pytanie("Kto napisał 'Romeo i Julia'?", new List<string> { "Charles Dickens", "Jane Austen", "William Shakespeare", "Mark Twain" }, 2),
+                new Pytanie("Który pierwiastek ma symbol 'O'?", new List<string> { "Oxygen", "Gold", "Silver", "Carbon" }, 0),
+                new Pytanie("Jaka jest waulta używana w Japonii?", new List<string> { "Yen", "Won", "Ringgit", "Baht" }, 0),
+                new Pytanie("Który kraj jest znany jako 'Kraj Kwitnącej Wiśni'?", new List<string> { "China", "Japan", "South Korea", "Vietnam" }, 1),
+                new Pytanie("Kto namalował Mona Lisę?", new List<string> { "Vincent van Gogh", "Leonardo da Vinci", "Pablo Picasso", "Claude Monet" }, 1),
+                new Pytanie("Jaki jest największy ocean na Ziemi?", new List<string> { "Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean" }, 3),
+                new Pytanie("Jaka jest stolica Australii?", new List<string> { "Sydney", "Melbourne", "Canberra", "Perth" }, 2),
+                new Pytanie("Który jest znany jako 'Ojciec informatyki''?", new List<string> { "Alan Turing", "Bill Gates", "Steve Jobs", "Ada Lovelace" }, 0)
+            };
+
+        }
+
+        
+
+        private void checkAnswer()
+        {
+            Pytanie pytanie1 = pytania[PytanieIndex];
+            if (OdpowiedzIndex == pytanie1.PytanieIndex)
+            {
+                
+                PytanieIndex += 1;
+                listBox1.SelectedIndex = pytania.Count - PytanieIndex;
+
+            }
+            else
+            {
+                MessageBox.Show($"Niepoprawna odpowiedź, koniec gry! \n \n Twój wynik: {listBox1.Items[pytania.Count - PytanieIndex].ToString()} $ ");
+                Close();
+            }
+        }
+
+        private void DrawVotes(object sender, PaintEventArgs e)
+        {
+
+            Pytanie pytanie1 = pytania[PytanieIndex];
+            int goodAnswer = pytanie1.PytanieIndex;
+
+            Random random = new Random();
+
+            int randomNumber1 = random.Next(0, 61);
+            int randomNumber2 = random.Next(0, 61 - randomNumber1);
+            int randomNumber3 = random.Next(0, 61 - randomNumber1 - randomNumber2);
+            int randomNumber4 = 61 - randomNumber1 - randomNumber2 - randomNumber3;
+
+            int h1 = randomNumber1;
+            int h2 = randomNumber2;
+            int h3 = randomNumber3;
+            int h4 = randomNumber4;
+
+            switch (goodAnswer)
+            {
+                case 0:
+                    h1 += 40;
+                    break;
+                case 1:
+                    h2 += 40;
+                    break;
+                case 2:
+                    h3 += 40;
+                    break;
+                case 3:
+                    h4 += 40;
+                    break;
+                default:
+                    break;
+            }
+
+
+            Graphics g = e.Graphics;
+
+
+            Pen pen = new Pen(Color.DodgerBlue, 2);
+
+
+            int x1 = 50;
+            int x2 = 200;
+            int x3 = 350;
+            int x4 = 500;
+
+            int y = 100;
+
+            int width = 100;
+
+            h1 *= 5;
+            h2 *= 5;
+            h3 *= 5;
+            h4 *= 5;
+
+
+            Font font = new Font("Arial", 10);
+            Brush brush = Brushes.Black;
+
+            int drawSpeed = 1000;
+
+            for (int i = 0; i < drawSpeed; i++)
+            {
+
+                {
+                    g.DrawRectangle(pen, x1, y, width, h1 * i / drawSpeed);
+                    g.DrawRectangle(pen, x2, y, width, h2 * i / drawSpeed);
+                    g.DrawRectangle(pen, x3, y, width, h3 * i / drawSpeed);
+                    g.DrawRectangle(pen, x4, y, width, h4 * i / drawSpeed);
+
+
+                    
+
+                    g.DrawString("A", font, brush, new PointF(x1 + width / 2 - 5, y - 25));
+                    g.DrawString($"{h1 / 5}%", font, brush, new PointF(x1 + width / 2 - 5, y + h1 + 5));
+                    g.DrawString("B", font, brush, new PointF(x2 + width / 2 - 5, y - 25));
+                    g.DrawString($"{h2 / 5}%", font, brush, new PointF(x2 + width / 2 - 5, y + h2 + 5));
+                    g.DrawString("C", font, brush, new PointF(x3 + width / 2 - 5, y - 25));
+                    g.DrawString($"{h3 / 5}%", font, brush, new PointF(x3 + width / 2 - 5, y + h3 + 5));
+                    g.DrawString("D", font, brush, new PointF(x4 + width / 2 - 5, y - 25));
+                    g.DrawString($"{h4 / 5}%", font, brush, new PointF(x4 + width / 2 - 5, y + h4 + 5));
+
+                }
+
+            }
+        }
+
+            private void button1_Click(object sender, EventArgs e)
+        {
+            OdpowiedzIndex = 0;
+            checkAnswer();
+            Form1_Load(sender, e);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            OdpowiedzIndex = 1;
+            Pytanie pytanie1 = pytania[PytanieIndex];
+            checkAnswer();
+            Form1_Load(sender, e);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OdpowiedzIndex = 2;
+            checkAnswer();
+            Form1_Load(sender, e);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OdpowiedzIndex = 3;
+            checkAnswer();
+            Form1_Load(sender, e);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Pytanie pytanie1 = pytania[PytanieIndex];
+            int goodAnswer = pytanie1.PytanieIndex;
+
+            switch (goodAnswer)
+            {
+                case 0:
+                    button2.Visible = false;
+                    button3.Visible = false;
+                    break;
+                case 1:
+                    button1.Visible = false;
+                    button3.Visible = false;
+                    break;
+                case 2:
+                    button1.Visible = false;
+                    button2.Visible = false;
+                    break;
+                case 3:
+                    button2.Visible = false;
+                    button3.Visible = false;
+                    break;
+                default:
+                    break;
+            }
+            button5.Visible = false;
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Gratulacje, wygrałeś: {listBox1.Items[pytania.Count - PytanieIndex].ToString()} $");
+            Close();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Form newForm = new Form();
+            newForm.Text = "Głosowanie widowni";
+
+            newForm.Size = new Size(650, 800);
+
+            newForm.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            newForm.Paint += DrawVotes;
+
+            newForm.ShowDialog();
+
+            button7.Visible = false;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
